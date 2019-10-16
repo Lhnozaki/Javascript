1a1fc1d41a2750c3461a51bcde402ba7f8ecea73
Lewis Nozaki <lhnozaki@gmail.com>
2019-10-15 22:47:11 -1000

    add form

--- angular/angular-todolist-demo/src/app/app.module.ts
+++ angular/angular-todolist-demo/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
-
+import { FormsModule } from "@angular/forms";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app/app.component";
 import { TodosComponent } from "./pages/todos/todos.component";
 import { TodoItemComponent } from "./pages/todo-item/todo-item.component";
-import { HeaderComponent } from './pages/layout/header/header.component';
+import { HeaderComponent } from "./pages/layout/header/header.component";
+import { AddComponent } from "./pages/add/add.component";
 
 @NgModule({
-  declarations: [AppComponent, TodosComponent, TodoItemComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    TodosComponent,
+    TodoItemComponent,
+    HeaderComponent,
+    AddComponent
+  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
