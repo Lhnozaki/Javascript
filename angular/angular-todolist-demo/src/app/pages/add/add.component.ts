07b3c6b967c3bba1e9aa0b2d62d1d9d3fcd70961
Lewis Nozaki <lhnozaki@gmail.com>
2019-10-15 23:16:26 -1000

    post

--- angular/angular-todolist-demo/src/app/pages/add/add.component.ts
+++ angular/angular-todolist-demo/src/app/pages/add/add.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, EventEmitter, Output } from "@angular/core";
 
 @Component({
   selector: "app-add",
@@ -6,9 +6,21 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./add.component.scss"]
 })
 export class AddComponent implements OnInit {
+  @Output() addTodo: EventEmitter<any> = new EventEmitter();
+
   title: string;
 
   constructor() {}
 
   ngOnInit() {}
+
+  onSubmit() {
+    const todo = {
+      title: this.title,
+      completed: false
+    };
+
+    console.log(todo.title);
+    this.addTodo.emit(todo);
+  }
 }
